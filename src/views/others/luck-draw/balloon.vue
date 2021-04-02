09aa4ebd1a660059ad914748b259377c48740a4b
strangerDemon <735359571@qq.com>
2021-04-02 22:02:43 +0800

    抽奖初始版本

--- src/views/others/luck-draw/balloon.vue
+++ src/views/others/luck-draw/balloon.vue
@@ -1,32 +1,51 @@
 <template>
-  <div class="balloon" :style="{'background-color':color}">
-    {{luckNumber}}
+  <div class="balloon" :style="{ 'background-color': color }">
+    {{ luckNumber }}
   </div>
 </template>
 <script lang="ts">
-import { onMounted, onUnmounted } from 'vue'
 export default {
-  name: 'balloon',
+  name: "balloon",
   props: {
     luckNumber: Number,
     color: String,
   },
   setup() {
-    //=>mounted
-    onMounted(() => {})
-    //=>unmounted
-    onUnmounted(() => {})
-    const methods = {}
-    return {}
+    return {};
   },
-}
+};
 </script>
 <style lang="less">
 .balloon {
+  position: relative;
+  top: 200px;
+  display: inline-block;
   color: #fff;
-  width: 10%;
+  width: 150px;
+  height: 150px;
+  border-radius: 75px;
   margin: 10px;
-  font-size: 20px;
+  font-size: 60px;
   font-weight: 700;
+  line-height: 150px;
+}
+.balloon::after {
+  content: " ";
+  width: 80px;
+  height: 60px;
+  border-radius: 50%;
+  position: absolute;
+  bottom: -30px;
+  left: 50%;
+  margin-left: -40px;
+  background: rgba(255, 255, 255, 0.1);
+  -webkit-box-shadow: 0 0 25px 20px rrgba(255, 255, 255, 0.1);
+  -moz-box-shadow: 0 0 25px 20px rgba(255, 255, 255, 0.1);
+  box-shadow: 0 0 25px 20px rgba(255, 255, 255, 0.1);
+  -webkit-transform: scaleY(0.3);
+  -moz-transform: scaleY(0.3);
+  -ms-transform: scaleY(0.3);
+  -o-transform: scaleY(0.3);
+  transform: scaleY(0.3);
 }
 </style>
--- src/views/others/luck-draw/index.vue
+++ src/views/others/luck-draw/index.vue
@@ -5,101 +5,98 @@
       <a-layout-content>
         <a-row class="luck-balloon">
           <a-col :span="18">
-            {{getRed()}}..
-            <balloon class="red-balloon" v-for="(balloon,index) in getRed()" :key="index" :lcukNumber="balloon"
-              :color="'red'">
-            </balloon>
+            <balloon
+              class="red-balloon"
+              v-for="(balloon, index) in redLuckBalloon"
+              :key="index"
+              :luckNumber="balloon"
+              :color="'red'"
+            ></balloon>
           </a-col>
           <a-col :span="6">
-            <balloon v-for="(balloon,index) in getBlue()" :key="index" :lcukNumber="balloon" :color="'blue'">
-            </balloon>
+            <balloon
+              v-for="(balloon, index) in blueLuckBalloon"
+              :key="index"
+              :luckNumber="balloon"
+              :color="'blue'"
+            ></balloon>
           </a-col>
         </a-row>
         <a-row class="operation">
-          <a-button class="reset-button" type="primary" @click="reset()"> again </a-button>
+          <a-button class="reset-button" type="primary" @click="reset()">
+            again
+          </a-button>
         </a-row>
       </a-layout-content>
       <a-layout-footer>
         design by demo &nbsp;<span>Vue 3.0 & TS & ant</span>&nbsp;
       </a-layout-footer>
     </a-layout>
-
   </div>
 </template>
 <script lang="ts">
-import { ref, onMounted, onUnmounted } from 'vue'
-import balloon from './balloon.vue'
+import { ref, onMounted, reactive } from "vue";
+import balloon from "./balloon.vue";
 export default {
-  name: 'luck-draw',
-  components: { balloon },
+  name: "luck-draw",
+  components: {
+    balloon,
+  },
   data() {
-    return {}
+    return {};
   },
   methods: {},
   setup() {
-    const RED_NUMBER: number = 6
-    const BLUE_NUMBER: number = 1
-    const MAX_RED_RANGE: number = 36
-    const MAX_BLUE_RANGE: number = 16
-
-    const RED_RANGE: Array<number> = []
-    const BLUE_RANGE: Array<number> = []
-
-    let redLuckBalloon: Array<number> = []
-    let blueLuckBalloon: Array<number> = []
-
-    //=>mounted
+    const RED_NUMBER: number = 6;
+    const BLUE_NUMBER: number = 1;
+    const MAX_RED_RANGE: number = 36;
+    const MAX_BLUE_RANGE: number = 16;
+    const RED_RANGE: number[] = [];
+    const BLUE_RANGE: number[] = [];
+    const redLuckBalloon = ref<number[]>();
+    const blueLuckBalloon = ref<number[]>();
     onMounted(() => {
-      reset()
-    })
-    //=>unmounted
-    onUnmounted(() => {})
+      reset();
+    });
     const methods = {
       init() {
-        redLuckBalloon = []
-        blueLuckBalloon = []
-
         for (let index = 0; index < MAX_RED_RANGE; index++) {
-          RED_RANGE[index] = index + 1
+          RED_RANGE[index] = index + 1;
         }
         for (let index = 0; index < MAX_BLUE_RANGE; index++) {
-          BLUE_RANGE[index] = index + 1
+          BLUE_RANGE[index] = index + 1;
         }
       },
-      getBalloon(
-        range: Array<number>,
-        order: number,
-        luckBalloon: Array<number>
-      ) {
+      getBalloon(range: number[], order: number, luckBalloon: number[]) {
         if (order === 0) {
-          return
+          return;
         }
-        let t = new Date().getTime()
-        let luckNumber = t % range.length
-        luckBalloon.push(range[luckNumber])
-        range.splice(luckNumber, 1)
-        this.getBalloon(range, order - 1, luckBalloon)
+        const luckNumber = new Date().getTime() % range.length;
+        luckBalloon.push(range[luckNumber]);
+        range.splice(luckNumber, 1);
+        this.getBalloon(range, order - 1, luckBalloon);
       },
-    }
-    const getRed = () => {
-      return redLuckBalloon
-    }
-    const getBlue = () => {
-      return blueLuckBalloon
-    }
+    };
     const reset = () => {
-      methods.init()
-      methods.getBalloon(RED_RANGE, RED_NUMBER, redLuckBalloon)
-      methods.getBalloon(BLUE_RANGE, BLUE_NUMBER, blueLuckBalloon)
-      redLuckBalloon.sort((a, b) => {
-        return a - b
-      })
-      console.log(redLuckBalloon, blueLuckBalloon)
-    }
-    return { getRed, getBlue, reset }
+      methods.init();
+      const redBalloon: number[] = [];
+      const blueBalloon: number[] = [];
+      methods.getBalloon(RED_RANGE, RED_NUMBER, redBalloon);
+      methods.getBalloon(BLUE_RANGE, BLUE_NUMBER, blueBalloon);
+      redBalloon.sort((a, b) => {
+        return a - b;
+      });
+      redLuckBalloon.value = redBalloon;
+      blueLuckBalloon.value = blueBalloon;
+    };
+    return {
+      redLuckBalloon,
+      blueLuckBalloon,
+      reset,
+    };
   },
-}
+};
 </script>
 <style lang="less">
-@import './index.less';
+@import "./index.less";
 </style>
